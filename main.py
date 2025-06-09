8dffa5953f69c91541cad1bb8b077674f3c822f7
Armand <armand.dewet40@gmail.com>
2025-06-09 11:16:02 +0200

    Added case handling for when search string has no matches

--- main.py
+++ main.py
@@ -62,55 +62,57 @@ query = st.text_input("Search for a product:")
 
 if query:
     matches = [i for i in product_titles if query.lower() in i.lower()]
-    selected = st.selectbox("Select from matches:", matches[:100])
-    st.write("You selected:", selected)
-
-    st.divider()
-
-    cpeName = cpe_df.loc[cpe_df['title'] == selected, 'cpeName'].values
-
-    if len(cpeName) > 1:
-        name = st.selectbox("Please select 1 to use as search criteria:", cpeName)
-
-    else:
-        name = cpeName[0]
-
-    st.write("The corresponding cpeName(s) are as follow:", name)
-    st.write("This search term will be used to find all known vulnerabilities associated with the product")
-
-    st.divider()
-
-    v_res = get_vulnerabilities(name)
-    if not v_res:
-        st.write('No vulnerabilities found')
-    else:
-        cve_dict, cve_metrics_dict = format_cve_data(v_res)
-        selected_cve_id = st.selectbox("Select vulnerability id:", list(cve_dict.keys()))
-
-        st.header('Vulnerability parameters')
-        st.write('Published: ',  cve_dict[selected_cve_id]['published'])
-        st.write('Last modified date: ',  cve_dict[selected_cve_id]['last modified'])
-        st.write('Vulnerability status: ',  cve_dict[selected_cve_id]['vulnerability status'])
-        st.write('Description: ',  cve_dict[selected_cve_id]['description'])
-        st.write('References: ',  cve_dict[selected_cve_id]['references'])
+    if matches:
+        selected = st.selectbox("Select from matches:", matches[:100])
+        st.write("You selected:", selected)
 
         st.divider()
-        if not cve_metrics_dict:
-            st.write("No metrics available")
+
+        cpeName = cpe_df.loc[cpe_df['title'] == selected, 'cpeName'].values
+
+        if len(cpeName) > 1:
+            name = st.selectbox("Please select 1 to use as search criteria:", cpeName)
+
         else:
-            if len(cve_metrics_dict[selected_cve_id].keys()) > 1:
-                selected_metric = st.selectbox("Select a metric to view:", list(cve_metrics_dict[selected_cve_id].keys()))
-            else: 
-                selected_metric = list(cve_metrics_dict[selected_cve_id].keys())[0]
+            name = cpeName[0]
 
-            st.write('Vulnerability metric name: ', selected_metric)
+        st.write("The corresponding cpeName(s) are as follow:", name)
+        st.write("This search term will be used to find all known vulnerabilities associated with the product")
 
-            # metric = cve_metrics_dict[selected_cve_id][selected_metric][0]
-            st.header('Vulnerability Metric', selected_metric)
-            # st.table(metric['cvssData'])
+        st.divider()
 
-            st.json(cve_metrics_dict[selected_cve_id][selected_metric])
-            
+        v_res = get_vulnerabilities(name)
+        if not v_res:
+            st.write('No vulnerabilities found')
+        else:
+            cve_dict, cve_metrics_dict = format_cve_data(v_res)
+            selected_cve_id = st.selectbox("Select vulnerability id:", list(cve_dict.keys()))
+
+            st.header('Vulnerability parameters')
+            st.write('Published: ',  cve_dict[selected_cve_id]['published'])
+            st.write('Last modified date: ',  cve_dict[selected_cve_id]['last modified'])
+            st.write('Vulnerability status: ',  cve_dict[selected_cve_id]['vulnerability status'])
+            st.write('Description: ',  cve_dict[selected_cve_id]['description'])
+            st.write('References: ',  cve_dict[selected_cve_id]['references'])
+
+            st.divider()
+            if not cve_metrics_dict:
+                st.write("No metrics available")
+            else:
+                if len(cve_metrics_dict[selected_cve_id].keys()) > 1:
+                    selected_metric = st.selectbox("Select a metric to view:", list(cve_metrics_dict[selected_cve_id].keys()))
+                else: 
+                    selected_metric = list(cve_metrics_dict[selected_cve_id].keys())[0]
+
+                st.write('Vulnerability metric name: ', selected_metric)
+
+                # metric = cve_metrics_dict[selected_cve_id][selected_metric][0]
+                st.header('Vulnerability Metric', selected_metric)
+                # st.table(metric['cvssData'])
+
+                st.json(cve_metrics_dict[selected_cve_id][selected_metric])
+    else:
+         st.write("No product mach your search string. Please search for a different product title")        
 
 
 else:
